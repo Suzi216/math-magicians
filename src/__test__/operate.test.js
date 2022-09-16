506ee896015911fc22643483c9b20ce03fc4dc1a
Suzi216 <66145033+Suzi216@users.noreply.github.com>
2022-09-16 13:54:01 +0200

    Adding testing for operation and calculate files with jest

---
+++ src/__test__/operate.test.js
@@ -0,0 +1,19 @@
+import operate from '../logic/operate';
+
+describe('Operating testing', () =>{
+  test("check adding", () =>{
+    expect(operate(2,2,"+")).toBe("4");
+  })
+  test("check multiplication", () =>{
+    expect(operate(2,2,"x")).toBe("4");
+  })
+  test("check division", () =>{
+    expect(operate(2,2,"-")).toBe("0");
+  })
+  test("check substraction", () =>{
+    expect(operate(2.2,2,"÷")).toBe("1.1");
+  })
+  test("check remaining", () =>{
+    expect(operate(2.2,2,"%")).toBe("0.2");
+  })
+})
