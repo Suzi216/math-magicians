9530c4ec69bfb24822c63cea9e389f62ad840cc8
skyv26 <dev.aakashv@gmail.com>
2022-09-17 15:28:07 +0530

    Remove unnecessary files

--- src/__test__/navbar.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Navbar from '../components/Navbar';
-
-it('renders correctly', () => {
-  const tree = renderer.create(<Navbar />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
