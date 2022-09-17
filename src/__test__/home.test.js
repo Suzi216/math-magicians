f3789807f2b710ceb093856002c83345ef1cd39d
skyv26 <dev.aakashv@gmail.com>
2022-09-17 15:16:06 +0530

    Resolve linter error

--- src/__test__/home.test.js
+++ src/__test__/home.test.js
@@ -1,6 +1,6 @@
-import Home from '../components/Home';
 import React from 'react';
 import renderer from 'react-test-renderer';
+import Home from '../components/Home';
 
 it('renders correctly', () => {
   const tree = renderer.create(<Home />).toJSON();
